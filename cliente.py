95d066edf60b9b619b35ea32259708a943039370
davibeltrao <davibeltrao02@gmail.com>
2017-12-01 05:27:19 -0200

    Add cliente/ Servent rodando e separando os dicionarios

---
+++ cliente.py
@@ -0,0 +1,16 @@
+import socket
+import sys
+
+class Cliente:
+	def __init__(self, Adress):
+
+		pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	cliente = Cliente(adress)
--- servent.py
+++ servent.py
@@ -7,22 +7,45 @@ class Servent:
 	def __init__(self, Adress):
 		self.con = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.con.bind(Adress)
+		self.keyDict = {}
+		self.neighbors = {}
 
-# Bind the socket to the port
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-while True:
-    print >>sys.stderr, '\nwaiting to receive message'
-    data, address = sock.recvfrom(4096)
-    
-    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    print >>sys.stderr, data
-    
-    if data:
-        sent = sock.sendto(data, address)
-        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+	def setDict(self, file):
+		with open(file, 'r') as f:
+			for i in f:
+				c = i.split()
+				#print(c[0])
+				if(c[0] == "#"):
+					continue
+				if c[0] not in self.keyDict:
+					self.keyDict[c[0]] = c[1]
+				else:
+					self.keyDict[c[0]] = c[1]
+			f.close()
+		print("RTMP = ", self.keyDict["rtmp"])
+		print("TCP MUX = ", self.keyDict["tcpmux"])
+		print("NBP = ", self.keyDict["nbp"])
+		print("COMPRESSNET = ", self.keyDict["compressnet"])
+
+	def loop(self):
+		while True:
+			data, adress = sock.recvfrom(414)
+			
+			pass
+
+
+#Bind the socket to the port
+# while True:
+#     print(sys.stderr, '\nwaiting to receive message')
+#     data, address = sock.recvfrom(414)
+#     if data:
+#         sent = sock.sendto(data, address)
+#         print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
 
 
 if __name__ == "__main__":
 	PORT = int(sys.argv[1])
 	ADRESS = ("", PORT)
-	servidor = Servent(ADRESS)
+	servent = Servent(ADRESS)
+	servent.setDict(sys.argv[2])
+	servent.loop()
